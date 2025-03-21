d62b3d14a7875af95539d6bfe174e122ef2255e3
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2025-03-22 03:57:55 +0800

    feat: add skeletal data to Model

--- include/Model/Model.h
+++ include/Model/Model.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <array>
+#include <unordered_map>
 #include <vector>
 #include <Model/Mesh.h>
+#include <Model/Animation.h>
 #include <string>
-
-
-
 
 class Model {
 public:
@@ -14,6 +13,10 @@ public:
     std::string source;
     std::string directory;
 
+    std::unordered_map<std::string, uint32_t> bone_to_index_map;
+    std::vector<Animation> registered_animations;
+    glm::mat4 global_inverse_transform = glm::mat4(1.0f);
+    Skeleton skeleton;
 };
 
 // Model getExampleModel();
