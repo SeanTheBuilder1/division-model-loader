834e4bcc26ce94f65fb8f079166fd0ac12555494
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2025-03-26 21:51:22 +0800

    feat: simpler matrix converter

--- src/model_loader.cpp
+++ src/model_loader.cpp
@@ -4,24 +4,7 @@
 #include <cinttypes>
 
 inline glm::mat4 aiMat4ToMat4(const aiMatrix4x4& matrix) {
-    glm::mat4 result;
-    result[0][0] = matrix.a1;
-    result[1][0] = matrix.a2;
-    result[2][0] = matrix.a3;
-    result[3][0] = matrix.a4;
-    result[0][1] = matrix.b1;
-    result[1][1] = matrix.b2;
-    result[2][1] = matrix.b3;
-    result[3][1] = matrix.b4;
-    result[0][2] = matrix.c1;
-    result[1][2] = matrix.c2;
-    result[2][2] = matrix.c3;
-    result[3][2] = matrix.c4;
-    result[0][3] = matrix.d1;
-    result[1][3] = matrix.d2;
-    result[2][3] = matrix.d3;
-    result[3][3] = matrix.d4;
-    return result;
+    return glm::transpose(glm::make_mat4(&matrix.a1));
 }
 
 std::string
