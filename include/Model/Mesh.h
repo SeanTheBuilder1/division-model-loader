4f93da91b12d5779be145e32ef05b05be8c7ec48
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2025-03-27 02:29:10 +0800

    feat: make tangent and bitangent empty by default

--- include/Model/Mesh.h
+++ include/Model/Mesh.h
@@ -13,8 +13,8 @@ struct Vertex {
     glm::vec3 position;
     glm::vec3 normal;
     glm::vec2 tex_coord;
-    glm::vec3 tangent;
-    glm::vec3 bitangent;
+    glm::vec3 tangent = glm::vec3(0.0f);
+    glm::vec3 bitangent = glm::vec3(0.0f);
     //
     int bone_ids[MAX_BONE_INFLUENCE] = {};
     float weights[MAX_BONE_INFLUENCE] = {};
